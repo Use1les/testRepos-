ec98ae32f9a81f8fcedc776a5edda3c6a45a46ab
Ilya Bogdanov <mopka9@gmail.com>
2024-03-13 22:47:11 +0300

    first array

--- firstRep.java
+++ firstRep.java
@@ -1,5 +1,12 @@
+import java.util.Arrays;
+
 public class firstRep {
-    public static void main (String[] args) {
-        System.out.println("Hello world!");
+public static void main (String[] args) {
+    int[] a = {4,6,7,78,98};
+    int[] b = {1,432,6,7,65,6,756};
+            System.out.println(Arrays.toString(a));
+    System.out.println();
+            System.out.println(Arrays.toString(b));
+
     }
 }
